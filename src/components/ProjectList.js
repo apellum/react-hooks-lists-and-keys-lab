79535c653e140646ada85f8da6c135f62fb599f2
Andrew Pellum <apellum19@gmail.com>
2021-07-07 17:51:21 -0600

    Completed Assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
+import user from "../data/user";
 
 function ProjectList({ projects }) {
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list"> {projects.map(project => <ProjectItem key={project.id} technologies={prop.technologies} name={props.name} about={props.about} /> )}
+      </div>
     </div>
   );
 }
